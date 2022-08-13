bc63ba97fc77e27d0a662c6a910ded4ea8015153
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-08-13 10:23:14 -0700

    Implement file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    App
-  </div>
-);
+import { Actors, MovieInfo, Movies, Profile, Navbar } from './index';
+import useStyles from './styles';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInfo />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieInfo = () => {
+  console.log('Movie Info');
+  return (
+    <div>
+      Movie Info
+    </div>
+  );
+};
+
+export default MovieInfo;
