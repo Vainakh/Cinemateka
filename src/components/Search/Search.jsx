60caea81ec3d3e2a3fe98772185cc1e294a5252a
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-08-17 06:26:53 -0700

    Implement search movie functionality

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,14 +3,19 @@ import { TextField, InputAdornment } from '@mui/material';
 import { Search as SearchIcon } from '@mui/icons-material';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { searchMovie } from '../../features/currentGenreOrCategory';
 import useStyles from './styles';
 
 const Search = () => {
   const classes = useStyles();
   const [query, setQuery] = useState('');
+  const dispatch = useDispatch();
 
-  const handleKeyPress = () => {
-
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      console.log('Here : ', query);
+      dispatch(searchMovie(query));
+    }
   };
 
   return (
