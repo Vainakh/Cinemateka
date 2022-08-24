647cd55647c341d9f83479a758aee35c87c32e78
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-08-24 07:32:28 -0700

    Fix number of movies per page pagination

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -9,6 +9,8 @@ const Movies = () => {
   const [page, setPage] = useState(1);
   const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
+  const numberOfMovies = lg ? 16 : 18;
 
   if (isFetching) {
     return (
@@ -41,7 +43,7 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} />
       <Pagination
         currentPage={page}
         setPage={setPage}
